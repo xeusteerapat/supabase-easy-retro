9bcf05cc8b398f0f19dc1e9b36b9e91bed07ca3a
xeusteerapat <xeus085@gmail.com>
2022-01-27 00:09:04 +0700

    :sparkles: [Update] add message function

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,80 @@
 import {
   Box,
-  Center,
+  Button,
   Container,
   Flex,
+  ListItem,
   Spacer,
   Text,
-  Input,
+  Textarea,
+  UnorderedList,
 } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import { supabase } from "../libs/supabase";
+import React, { EventHandler } from "react";
+import Navbar from "../components/Navbar";
+import { Message } from "../types/message";
+
+const sampleList = [
+  {
+    id: 1,
+    message: "Hi, there",
+  },
+  {
+    id: 2,
+    message: "Tower",
+  },
+  {
+    id: 3,
+    message: "Long time no see",
+  },
+  {
+    id: 4,
+    message: "I don't know",
+  },
+];
 
 const Home: NextPage = () => {
+  const [inputValue, setInputValue] = React.useState("");
+  const [myList, setMyList] = React.useState(sampleList);
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setMyList([...myList, { id: Date.now(), message: inputValue }]);
+    setInputValue("");
+  };
+
   return (
     <Container maxW='6xl'>
-      <Center>
-        <Text fontSize='4xl'>(Not) EazyRetro</Text>
-      </Center>
+      <Navbar />
       <Flex>
-        <Box w='350px' h='1000' bg='gray.100'>
+        <Box w='400px' h='1000' bg='gray.100'>
           <Text fontSize='2xl'>Went Well</Text>
-          <Input
-            placeholder='Basic usage'
+          <Textarea
+            placeholder='Describe your thought'
             variant='outline'
             backgroundColor='white'
+            onChange={handleChange}
+            value={inputValue}
           />
+          <Button onClick={handleSubmit}>ADD</Button>
+          <UnorderedList>
+            {!myList.length
+              ? null
+              : myList.map((item: Message) => (
+                  <ListItem key={item.id}>{item.message}</ListItem>
+                ))}
+          </UnorderedList>
         </Box>
         <Spacer />
-        <Box w='350px' h='1000' bg='gray.100'>
+        <Box w='400px' h='1000' bg='gray.100'>
           <Text fontSize='2xl'>To Improve</Text>
         </Box>
         <Spacer />
-        <Box w='350px' h='1000' bg='gray.100'>
+        <Box w='400px' h='1000' bg='gray.100'>
           <Text fontSize='2xl'>Action Items</Text>
         </Box>
       </Flex>
