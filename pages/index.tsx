b9bfcf66ece9422113eae8c2d97e0167029b8cf8
xeusteerapat <xeus085@gmail.com>
2022-01-29 19:43:19 +0700

    :sparkles: [Update] realtime fetching data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,97 +1,106 @@
-import {
-  Box,
-  Button,
-  Container,
-  Flex,
-  ListItem,
-  Spacer,
-  Text,
-  Textarea,
-  UnorderedList,
-} from "@chakra-ui/react";
+import { Box, Container, Flex, Spacer, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useMemo } from "react";
+import MessageCard from "../components/MessageCard";
 import Navbar from "../components/Navbar";
-import { Message } from "../types/message";
+import TextAreaInput from "../components/TextAreaInput";
 import { supabase } from "../libs/supabase";
 import { User } from "../types/user";
 
-const sampleList = [
-  {
-    id: 1,
-    message: "Hi, there",
-  },
-  {
-    id: 2,
-    message: "Tower",
-  },
-  {
-    id: 3,
-    message: "Long time no see",
-  },
-  {
-    id: 4,
-    message: "I don't know",
-  },
-];
-
 const Home: NextPage = () => {
   const [inputValue, setInputValue] = React.useState("");
-  const [myList, setMyList] = React.useState(sampleList);
   const [currentUser, setCurrentUser] = React.useState<User | null>(null);
+  const [data, setData] = React.useState<any[] | null>([]);
+  const [errorText, setErrorText] = React.useState("");
+  const router = useRouter();
 
   const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setInputValue(e.target.value);
   };
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    setMyList([...myList, { id: Date.now(), message: inputValue }]);
-    setInputValue("");
-  };
-
   React.useEffect(() => {
     const user = supabase.auth.user();
+
+    if (!user) {
+      router.push("/signin");
+    }
 
     setCurrentUser({ ...currentUser, id: user?.id, email: user?.email });
     // eslint-disable-next-line
   }, []);
 
-  return (
-    <Container maxW='6xl'>
-      <Navbar user={currentUser} />
-      <Flex>
-        <Box w='400px' h='1000' bg='gray.100'>
-          <Text fontSize='2xl'>Went Well</Text>
-          <Textarea
-            placeholder='Describe your thought'
-            variant='outline'
-            backgroundColor='white'
-            onChange={handleChange}
-            value={inputValue}
-          />
-          <Button onClick={handleSubmit} colorScheme='blue'>
-            ADD
-          </Button>
-          <UnorderedList>
-            {!myList.length
+  const fetchData = async () => {
+    const { data, error } = await supabase.from("went_well").select();
+
+    if (error) {
+      setErrorText(error.message);
+    }
+
+    setData(data);
+  };
+
+  React.useEffect(() => {
+    fetchData();
+    const mySubscription = supabase
+      .from("went_well")
+      .on("*", () => fetchData())
+      .subscribe();
+
+    return () => supabase.removeSubscription(mySubscription);
+  }, []);
+
+  const fetchLastestMessages = useMemo(() => {
+    const handleSubmit = async (e: React.FormEvent) => {
+      e.preventDefault();
+
+      try {
+        await supabase
+          .from("went_well")
+          .insert({ message: inputValue, user_id: currentUser?.id });
+      } catch (error) {
+        console.log(error);
+        if (error instanceof Error) {
+          setErrorText(error.message);
+        }
+      }
+
+      setInputValue("");
+    };
+
+    if (errorText) return <p>Oh no... {errorText}</p>;
+
+    return (
+      <Container maxW='6xl'>
+        <Navbar user={currentUser} />
+        <Flex>
+          <Box w='350px' h='1000' bg='gray.100'>
+            <Text fontSize='2xl'>Went Well</Text>
+            <TextAreaInput
+              handleChange={handleChange}
+              handleSubmit={handleSubmit}
+              inputValue={inputValue}
+            />
+            {!data?.length
               ? null
-              : myList.map((item: Message) => (
-                  <ListItem key={item.id}>{item.message}</ListItem>
+              : data?.map((item: any) => (
+                  <MessageCard key={item.id} message={item.message} />
                 ))}
-          </UnorderedList>
-        </Box>
-        <Spacer />
-        <Box w='400px' h='1000' bg='gray.100'>
-          <Text fontSize='2xl'>To Improve</Text>
-        </Box>
-        <Spacer />
-        <Box w='400px' h='1000' bg='gray.100'>
-          <Text fontSize='2xl'>Action Items</Text>
-        </Box>
-      </Flex>
-    </Container>
-  );
+          </Box>
+          <Spacer />
+          <Box w='350px' h='1000' bg='gray.100'>
+            <Text fontSize='2xl'>To Improve</Text>
+          </Box>
+          <Spacer />
+          <Box w='350px' h='1000' bg='gray.100'>
+            <Text fontSize='2xl'>Action Items</Text>
+          </Box>
+        </Flex>
+      </Container>
+    );
+  }, [currentUser, data, errorText, inputValue]);
+
+  return fetchLastestMessages;
 };
 
 export default Home;
